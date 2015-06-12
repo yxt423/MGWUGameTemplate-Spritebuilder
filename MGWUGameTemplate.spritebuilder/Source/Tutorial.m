0b0f2c1a12e0a0dfd16a6c7de069b111ea97f3cd
Xintong Yu <yxt423@gmail.com>
2015-06-12 20:09:31 +0800

    tutorial step 3

--- MGWUGameTemplate.spritebuilder/Source/Tutorial.m
+++ MGWUGameTemplate.spritebuilder/Source/Tutorial.m
@@ -7,12 +7,16 @@
 //
 
 #import "Tutorial.h"
+#import "GamePlay+UIUtils.h"
 #import "GameManager.h"
 #import "Character.h"
+#import "Star.h"
+#import "Cloud.h"
 
 @implementation Tutorial {
     int _tutorialState;
     float _flagPosition;
+    CCNode *_tutorialText;
 }
 
 - (id)init {
@@ -20,8 +24,9 @@
     if (!self) return(nil);
     
     _tutorialState = 0;
-    // 0: to load left cover.  1, left cover loaded.  2, right cover loaded.  3,
-    
+    /* 0: to load left cover.  1, left cover loaded.  2, right cover loaded.  
+     3, clouds and star loaded. 4, finish!
+    */
     
     return self;
 }
@@ -35,8 +40,7 @@
 - (void)update:(CCTime)delta {
     switch (_tutorialState) {
         case 0:
-            [self loadCoverOnLeft];
-            _tutorialState = 1;
+            [self tutorialStep1];
             break;
         case 1:
             break;
@@ -65,17 +69,66 @@
     }
 }
 
-- (void)loadCoverOnLeft {
+/* collision functions */
+
+// switching states when the character hit the groud.
+- (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair character:(CCNode *)nodeA groud:(CCNode *)nodeB {
+    [_character jump];
+    
+    if (_tutorialState == 1 && _character.position.x > _flagPosition + 70) {
+        [self tutorialStep2];
+    } else if (_tutorialState == 2 && _character.position.x + 70 < _flagPosition) {
+        [self tutorialStep3];
+    }
+    
+    return YES;
+}
+
+- (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair character:(CCNode *)nodeA star:(CCNode *)nodeB {
+    [GameManager playThenCleanUpAnimationOf:_tutorialText Named:@"Out"];
+    
+    _starHit += 1;
+    score *= 2;
+    [super updateScore];
+    
+    [_character jump];
+    CGPoint collisionPoint = pair.contacts.points[0].pointA;
+    [(Star *)nodeB removeAndPlayAnimationAt:(CGPoint)collisionPoint];
+    
+    return YES;
+}
+
+-(BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair character:(CCNode *)nodeA cloud:(CCNode *)nodeB {
+    _gameManager.cloudHit += 1;
+    score += _gameManager.cloudHit * 10;
+    [self updateScore];
+    [_character jump];
+    [(Cloud *)nodeB removeAndPlayAnimation];
+    return YES;
+}
+
+/* tutorial steps */
+
+- (void)tutorialStep1 {
+    _tutorialState = 1;
+    
     pauseCover = [CCBReader load:@"Gadgets/PauseCover"];
     pauseCover.anchorPoint = CGPointMake(1, 0);
     pauseCover.position = CGPointMake(_character.position.x, 0);
     [self addChild:pauseCover];
     _flagPosition = _character.position.x;
     
-    [self loadTabAt: ccp((_gameManager.screenWidth - _character.position.x) / 2 + _character.position.x, 80)];
+    float positionX = (_gameManager.screenWidth - _character.position.x) / 2 + _character.position.x;
+    [self loadTabAt: ccp(positionX, 80)];
+    _tutorialText = [GameManager addCCNodeFromFile:@"Gadgets/TutorialText1" WithPosition:ccp(positionX, 200) To:self];
 }
 
-- (void)loadCoverOnRight {
+- (void)tutorialStep2 {
+    // clean up previous step; load cover, load tabPosition, load text.
+    _tutorialState = 2;
+    [GameManager playThenCleanUpAnimationOf:pauseCover Named:@"Out"];
+    [GameManager playThenCleanUpAnimationOf:_tutorialText Named:@"Out"];
+    
     pauseCover = [CCBReader load:@"Gadgets/PauseCover"];
     pauseCover.anchorPoint = CGPointMake(0, 0);
     pauseCover.position = CGPointMake(_character.position.x, 0);
@@ -83,25 +136,25 @@
     _flagPosition = _character.position.x;
     
     [self loadTabAt:ccp(_character.position.x / 2, 80)];
+    _tutorialText = [GameManager addCCNodeFromFile:@"Gadgets/TutorialText2" WithPosition:ccp(_character.position.x / 2, 200) To:self];
+}
+
+- (void)tutorialStep3 {
+    // clean up previous step; load text, load cloud and star.
+    _tutorialState = 3;
+    [GameManager playThenCleanUpAnimationOf:pauseCover Named:@"Out"];
+    [GameManager playThenCleanUpAnimationOf:_tutorialText Named:@"Out"];
+    
+    _tutorialText = [GameManager addCCNodeFromFile:@"Gadgets/TutorialText3" WithPosition:ccp(_gameManager.screenWidth / 2, _gameManager.screenHeight * 0.7) To:self];
+    
+    CCNode *cloud = [GameManager addCCNodeFromFile:@"Objects/Cloud" WithPosition:ccp(_gameManager.screenWidth - 130, 120) To:_objectsGroup];
+    [GameManager addCCNodeFromFile:@"Objects/Cloud" WithPosition:ccp(cloud.position.x - 50, 180) To:_objectsGroup];
+    [GameManager addCCNodeFromFile:@"Objects/StarStatic" WithPosition:ccp(cloud.position.x, 240) To:_objectsGroup];
 }
 
 - (void)loadTabAt: (CGPoint)position {
     CCNode *T_tab = [GameManager addCCNodeFromFile:@"Gadgets/TutorialTab" WithPosition:position To:self];
     [GameManager playThenCleanUpAnimationOf:T_tab Named:@"In"];
-}
-
-// switching states when the character hit the groud. 
-- (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair character:(CCNode *)nodeA groud:(CCNode *)nodeB {
-    [_character jump];
-    if (_tutorialState == 1 && _character.position.x > _flagPosition + 70) {
-        [GameManager playThenCleanUpAnimationOf:pauseCover Named:@"Out"];
-        [self loadCoverOnRight];
-        _tutorialState = 2;
-    } else if (_tutorialState == 2 && _character.position.x + 70 < _flagPosition) {
-        [GameManager playThenCleanUpAnimationOf:pauseCover Named:@"Out"];
-        _tutorialState = 3;
-    }
-    return YES;
 }
 
 -(void)swipeUpGesture:(UISwipeGestureRecognizer *)recognizer{
